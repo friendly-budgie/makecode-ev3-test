eb8266f937c549b9980d47b8a7900840b3b209b9
Laen <github1@denizen.org>
2020-07-13 21:12:32 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,9 @@ function doABarrelRoll (_3: number) {
     motors.largeBC.steer(0, 50, _3, MoveUnit.Rotations)
     pause(100)
 }
+sensors.touch1.onEvent(ButtonEvent.Pressed, function () {
+    music.playSoundEffect(sounds.mechanicalHorn1)
+})
 brick.setStatusLight(StatusLight.Orange)
 forever(function () {
     music.setVolume(100)
