6981f9d4158fa102412d223d8ee48682e3c33a5a
Laen <github1@denizen.org>
2020-07-13 21:07:09 -0700

    Wootwoot

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+function doABarrelRoll (_3: number) {
+    motors.largeBC.steer(0, 50, _3, MoveUnit.Rotations)
+}
 brick.setStatusLight(StatusLight.Orange)
 forever(function () {
     music.setVolume(100)
